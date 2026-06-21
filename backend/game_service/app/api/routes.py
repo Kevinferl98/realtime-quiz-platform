8d1ebde6271f8ca0b631faa35862802e2cbe047d
Kevin Ferlauto <kevinferl1998@gmail.com>
2026-06-21 11:53:47 +0200

    refactor(game_service): refactor user authentication
    - Move user authentication logic into app/dependencies.py.
    - Create app/core/security.py to handle JWT decoding using PyJWKClient
    - Remove legacy auth.py module.
    - Refactor unit tests to use the new get_current_user dependency.

--- backend/game_service/app/api/routes.py
+++ backend/game_service/app/api/routes.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi import APIRouter, Depends, status, Request
 from app.services import room_service
-from app.auth import get_current_user
-from app.dependencies import get_redis_client
+from app.dependencies import get_redis_client, get_current_user
 from app.schemas.multiplayer import RoomCreateResponse
 from my_observability import get_logger
 
@@ -15,9 +14,6 @@ router = APIRouter(prefix="/game", tags=["game"])
     summary="Create a new room based on a quiz",
 )
 async def create_room(quiz_id: str, request: Request, user=Depends(get_current_user), redis=Depends(get_redis_client)):
-    if user is None:
-        raise HTTPException(status_code=401, detail="User not logged in")
-
     quiz_client = request.app.state.quiz_client
 
     return await room_service.create_room(redis, quiz_id, user["sub"], quiz_client)
